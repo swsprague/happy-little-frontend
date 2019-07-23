8bd18e9a3f5d8879b509912215d51eedda40fb78
sethwsprague <sethwsprague@gmail.com>
2019-07-22 21:44:13 -0400

    Add Heroku URL to Config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://happy-little-backend.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
