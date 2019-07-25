704e21646e295d4e99fd6f7ecdb844e1d9dc3583
sethwsprague <sethwsprague@gmail.com>
2019-07-25 17:28:08 -0400

    Remove forward slash from config heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://happy-little-backend.herokuapp.com/',
+  production: 'https://happy-little-backend.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
